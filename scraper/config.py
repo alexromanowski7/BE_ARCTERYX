12585e3b631d45bd117d424a61eb28c3d261fbdf
Alex Romanowski <romanowski7x@gmail.com>
2025-11-16 18:48:04 +0100

    Add Scraper

---
+++ scraper/config.py
@@ -0,0 +1,15 @@
+# scraper/config.py
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0 Safari/537.36"
+    )
+}
+
+# Główny indeks sitemap z robots.txt
+SITEMAP_INDEX_URL = "https://arcteryx.com/sitemap.xml"
+
+# Szukamy tylko URL-i produktów ze sklepu US/EN
+VALID_ROOT = "/us/en/shop/"
